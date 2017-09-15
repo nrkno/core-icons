212075a8911fa06543535520b315eb256360b2b6
Eirik Backer <eirik.backer@gmail.com>
2017-09-15 20:54:27 +0200

    Update svg-to-js to aviod adblock issues, Remove excess dependencies (#24)

--- bin/build.js
+++ bin/build.js
@@ -1,39 +1,16 @@
-/* eslint-disable no-console, no-process-exit */
-const chalk = require('chalk');
-const path = require('path');
-
 const concatSvg = require('@nrk/svg-to-js');
-const parseArgs = require('minimist');
+const path = require('path');
+const fs = require('fs');
 
-const yearReplacePattern = '||YEAR_FOR_COPYRIGHT||';
-
-const defaultOptions = {
+const config = {
   svgFileName: 'core-icons.js',
   svgFileNameMin: 'core-icons.min.js',
   srcPath: path.join(__dirname, '../src'),
   distPath: path.join(__dirname, '../dist'),
-  banner: `Copyright (c) 2015-${yearReplacePattern} NRK <opensource@nrk.no>`
+  banner: `Copyright (c) 2015-${new Date().getFullYear()} NRK <opensource@nrk.no>`
 };
 
-console.log(chalk.yellow('Building SVG'));
-const argv = parseArgs(process.argv.slice(2), {
-  string: [
-    'svgFileName',
-    'svgFileNameMin',
-    'srcPath',
-    'distPath',
-    'banner'
-  ],
-  default: defaultOptions,
-  unknown: () => {
-    console.log(`Passed in invalid options: ${chalk.styles.red.open}${process.argv.slice(2)}${chalk.styles.red.close}`);
-    console.log('Usage: $ node build.js ');
-    Object.keys(defaultOptions).map((key) => console.log(`  --${key} ${defaultOptions[key]}`));
-    console.log('');
-    process.exit(1);
-  }
-});
-argv.banner = argv.banner.replace(yearReplacePattern, new Date().getFullYear());
-concatSvg(argv)
-  .then(() => console.log(chalk.green('Merged and minified SVG')))
+console.log('Building SVG');
+concatSvg(config)
+  .then(() => console.log('Merged and minified SVG'))
   .catch((err) => console.log(err.stack));
