10ade089932f793a0b748478575b1d2398c7aad6
Eirik Backer <eirik.backer@gmail.com>
2018-08-17 08:26:39 +0200

    Convert to IIFE module avoiding require.js conflicts, CJS for NPM , fix tilgjengelighet rendering bug and add upload icon (#59)

--- rollup.config.js
+++ rollup.config.js
@@ -1,46 +1,72 @@
 import {uglify} from 'rollup-plugin-uglify'
 import buble from 'rollup-plugin-buble'
 import serve from 'rollup-plugin-serve'
+import pkg from './package.json'
 import fs from 'fs'
 
+const banner = `/*! ${pkg.name} v${pkg.version} - Copyright (c) 2017-${new Date().getFullYear()} NRK */`
 const globals = {'react-dom': 'ReactDOM', react: 'React'} // Do not include react in out package
-const plugins = [buble(), uglify(), !process.env.ROLLUP_WATCH || serve('lib')]
+const pluginsCJS = [buble(), !process.env.ROLLUP_WATCH || serve('lib')]
+const pluginsMIN = pluginsCJS.concat(uglify({output: {comments: /^!/}}))
 
 export default [{
+  watch: {include: 'lib/*.(svg|js)'},
   input: 'lib/core-icons.js',
   output: {
-    file: 'lib/core-icons.min.js',
-    format: 'umd',
     name: 'coreIcons',
+    file: 'lib/core-icons.min.js', // IIFE for static.nrk.no
+    format: 'iife',
     sourcemap: true,
-    intro
+    intro,
+    banner
   },
-  watch: {include: 'lib/*.(svg|js)'},
-  plugins
+  plugins: pluginsMIN
+}, {
+  input: 'lib/core-icons.js',
+  output: {
+    name: 'coreIcons',
+    file: 'lib/core-icons.cjs.js', // CJS for vanilla NPM install
+    format: 'cjs',
+    intro,
+    banner
+  },
+  plugins: pluginsCJS
 }, {
   input: 'lib/core-icons.jsx',
   output: {
-    file: 'jsx/index.js',
-    format: 'umd',
     name: 'CoreIcon',
+    file: 'lib/core-icons.jsx.js', // UDM for JSX documentation
+    format: 'umd',
     sourcemap: true,
     intro,
+    banner,
     globals
   },
   external: Object.keys(globals),
-  plugins
+  plugins: pluginsMIN
+}, {
+  input: 'lib/core-icons.jsx', // CJS for JSX NPM install
+  output: {
+    name: 'CoreIcon',
+    file: 'jsx.js',
+    format: 'cjs',
+    intro,
+    banner,
+    globals
+  },
+  external: Object.keys(globals),
+  plugins: pluginsCJS
 }]
 
 function intro () {
   const files = fs.readdirSync('./lib').filter((file) => file.slice(-4) === '.svg')
   const icons = files.map((file) => {
     const code = String(fs.readFileSync(`./lib/${file}`))
-    const body = code.replace(/^[^>]+>|<[^<]+$/g, '').replace(/\s*([<>])\s*/g, '$1') // Strip white space around tokens
     const size = String(code.match(/viewBox="[^"]+/)).split(' ').map(Number)
+    const body = code.replace(/^[^>]+>|<[^<]+$/g, '').replace(/\s*([<>])\s*/g, '$1') // Strip white space around tokens
     return `'${file.slice(0, -4)}':['${body}',${size[2]},${size[3]}]` // Generate JS instead of JSON to save bytes
   })
 
   fs.writeFileSync('./lib/core-icons.json', JSON.stringify(files))
-
   return `var ICONS = {${icons.join(',')}}`
 }
