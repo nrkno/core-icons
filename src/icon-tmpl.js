849641673715158720f0a8ebceed82932640f35e
Alexander Pope <alex@pope-industries.com>
2018-10-12 11:40:31 +0200

    replace renderAs* functions

--- src/icon-tmpl.js
+++ src/icon-tmpl.js
@@ -1,51 +1,18 @@
-export { renderAsSymbol, renderAsSprite, renderAsSVG };
-
-const BODY = '{{BODY}}';
-const HEIGHT = '{{HEIGHT}}';
-const ID = '{{ID}}';
-const WIDTH = '{{WIDTH}}';
-
-/**
- * Render icon as symbol definition
- */
-function renderAsSymbol() {
-  return '<symbol id=\"'
-    + ID
-    + '\" viewBox=\"0 0 '
-    + WIDTH
-    + ' '
-    + HEIGHT
-    + '\">'
-    + BODY
-    + '</symbol>';
-}
-
-/**
- * Render icon as sprite (referencing symbol definition)
- */
-function renderAsSprite() {
-  return '<svg style=\"width:'
-    + (WIDTH / 10)
-    + 'em;height:'
-    + (HEIGHT / 10)
-    + 'em\" focusable=\"false\" aria-hidden=\"true\"><use xlink:href=\"#'
-    + ID
-    + '\" /></svg>';
-}
+var BODY = '{{BODY}}';
+var HEIGHT = '{{HEIGHT}}';
+var WIDTH = '{{WIDTH}}';
 
 /**
  * Render icon as independant SVG element
  */
-function renderAsSVG() {
-  return '<svg style=\"width:'
-    + (WIDTH / 10)
-    + 'em;height:'
-    + (HEIGHT / 10)
-    + 'em\" viewBox=\"0 0 '
-    + WIDTH
-    + ' '
-    + HEIGHT
-    + '\" focusable=\"false\" aria-hidden=\"true\">'
-    + BODY
-    + '</svg>';
-}
+export default '<svg style=\"width:'
+  + (WIDTH / 10)
+  + 'em;height:'
+  + (HEIGHT / 10)
+  + 'em\" viewBox=\"0 0 '
+  + WIDTH
+  + ' '
+  + HEIGHT
+  + '\" focusable=\"false\" aria-hidden=\"true\">'
+  + BODY
+  + '</svg>';
--- src/icon-tmpl.jsx
+++ src/icon-tmpl.jsx
@@ -1,41 +1,13 @@
 import React from 'react'
 
-export { renderAsSymbol, renderAsSprite, renderAsSVG };
-
-const BODY = '{{BODY}}';
-const HEIGHT = '{{HEIGHT}}';
-const ID = '{{ID}}';
-const WIDTH = '{{WIDTH}}';
-
-/**
- * Render icon as symbol definition
- */
-function renderAsSymbol() {
-  return React.createElement('symbol', {
-    dangerouslySetInnerHTML: { __html: BODY },
-    id: ID,
-    viewbox: '0 0 ' + WIDTH + ' ' + HEIGHT
-  });
-}
-
-/**
- * Render icon as sprite (referencing symbol definition)
- */
-function renderAsSprite() {
-  return React.createElement('svg', {
-    'aria-hidden': true,
-    focusable: false,
-    style: {
-      height: '' + (HEIGHT / 10) + 'em',
-      width: '' + (WIDTH / 10) + 'em'
-    }
-  }, [React.createElement('use', { 'xlink:href': '#' + ID })]);
-}
+var BODY = '{{BODY}}';
+var HEIGHT = '{{HEIGHT}}';
+var WIDTH = '{{WIDTH}}';
 
 /**
  * Render icon as independant SVG element
  */
-function renderAsSVG() {
+export default function renderSVG() {
   return React.createElement('svg', {
     'aria-hidden': true,
     dangerouslySetInnerHTML: { __html: BODY },
