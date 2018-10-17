2f441f92c4fb3a444ec95ccac83c2fd5fb1bd88c
Alexander Pope <alex@pope-industries.com>
2018-10-17 12:24:56 +0200

    store [body, width, height] in global map

--- src/icon-tmpl.js
+++ src/icon-tmpl.js
@@ -1,8 +1,1 @@
-var BODY = '{{BODY}}'
-var HEIGHT = '{{HEIGHT}}'
-var WIDTH = '{{WIDTH}}'
-
-/**
- * Render icon as independant SVG element
- */
-{{GLOBAL}}['{{ID}}'] = `<svg style="width:${WIDTH / 10}em;height:${HEIGHT / 10}em" viewBox="0 0 ${WIDTH} ${HEIGHT}" focusable="false" aria-hidden="true">${BODY}</svg>`
+{{GLOBAL}}['{{ID}}'] = ['{{BODY}}', {{WIDTH}}, {{HEIGHT}}]
