802b6700fca21dbcb2d3cf9a12b18007b3a65244
Eirik Backer <Eirik.Backer@nrk.no>
2018-10-22 16:16:59 +0200

    Generate without Rollup and require tree shaking

--- bin/build.js
+++ bin/build.js
@@ -7,8 +7,9 @@ const icons = fs.readdirSync('lib').map((file) => {
   const [x, y, w, h] = String(code.match(/viewBox="[^"]+/)).split(' ').map(Number)
   const body = code.replace(/^[^>]+>|<[^<]+$/g, '').replace(/\s*([<>])\s*/g, '$1') // Strip white space around tokens
   const svg = `<svg style="width:${w / 10}em;height:${h / 10}em" viewBox="0 0 ${w} ${h}" focusable="false" aria-hidden="true">${body}</svg>`
-  const key = id.replace(/-+./g, (m) => m.slice(-1).toUpperCase())
-  return {id, body, key, svg, x, y, w, h}
+  const key = id.replace(/-+./g, (m) => m.slice(-1).toUpperCase()) // camelCase
+  const jsx = key.replace(/./, (m) => m.toUpperCase()) // TitleCamelCase
+  return { id, body, key, jsx, svg, x, y, w, h }
 }).filter(Boolean) // Remove falsy values
 
 generateSketch()
@@ -16,37 +17,47 @@ generateDocs()
 generateJSON()
 generateIIFE()
 generateMJS()
+generateCJS()
 generateJSX()
 
 function generateJSON () {
-  fs.writeFileSync('lib/core-icons.json', JSON.stringify(icons.map(({id}) => `${id}.svg`)))
+  fs.writeFileSync('lib/core-icons.json', JSON.stringify(icons.map(({ id }) => `${id}.svg`)))
+}
+
+function generateCJS () {
+  fs.writeFileSync('core-icons.cjs', `module.exports = {${icons.map(({ key, svg }) => `'${key}':'${svg}'`).join(',')}}`)
 }
 
 function generateMJS () {
-  fs.writeFileSync('core-icons.mjs', icons.map(({key, svg}) => `export const ${key} = '${svg}'`).join('\n'))
+  fs.writeFileSync('core-icons.mjs', icons.map(({ key, svg }) => `export const ${key} = '${svg}'`).join('\n'))
 }
 
 function generateIIFE () {
-  const symbols = icons.map(({id, w, h, body}) => `<symbol id="${id}" viewBox="0 0 ${w} ${h}">${body}</symbol>`)
-  const script = `if(typeof document !== 'undefined'){
+  const symbols = icons.map(({ id, w, h, body }) => `<symbol id="${id}" viewBox="0 0 ${w} ${h}">${body}</symbol>`)
+  const script = `if (typeof document !== 'undefined') {
+  window.coreIcons = function () {
+    console.warn('Using window.coreIcons is deprecated')
+    return {}
+  }
   var div = document.createElement('div')
-  div.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" style="display:none">${symbols}</svg>'
+  div.innerHTML = '<svg data-core-icons="${pkg.version}" xmlns="http://www.w3.org/2000/svg" style="display:none">${symbols}</svg>'
   document.head.appendChild(div.firstElementChild)
-}`
+}\n`
   fs.writeFileSync('lib/core-icons.min.js', script)
   fs.writeFileSync('lib/core-icons.js', script)
 }
 
 function generateJSX () {
   fs.writeFileSync('core-icons.jsx', `import React from 'react'
-${icons.map(({body, key, w, h}) => `export function ${key} () {
-  return React.createElement('svg', {
-    'aria-hidden': true,
-    focusable: false,
-    viewbox: '0 0 ${w} ${h}',
-    style: {width: '${w / 10}em', height: '${h / 10}em'},
-    dangerouslySetInnerHTML: {__html: '${body}'}
-  })
+${icons.map(({ body, jsx, w, h }) => `export function ${jsx} (attr) {
+  return React.createElement('svg', Object.keys(attr=attr||{})
+    .reduce(function(acc,key){acc[key]=attr[key];return acc}, {
+      'aria-hidden': true,
+      focusable: false,
+      viewbox: '0 0 ${w} ${h}',
+      style: {width: '${w / 10}em', height: '${h / 10}em'},
+      dangerouslySetInnerHTML: {__html: '${body}'}
+    }))
 }`.replace(/\n|\s{2,}/g, '')).join('\n')}`)
 }
 
