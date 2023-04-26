d8e0004537ada364da833d9a37f247e193a5fc88
Skjalg Teig <skjalgepalg@gmail.com>
2023-04-26 15:50:58 +0300

    chore: Add try/catch to svgToPdf in build script

--- bin/build.js
+++ bin/build.js
@@ -94,7 +94,11 @@ function buildIcons (groupName) {
   // Make pdfs
   const svgFiles = fse.readdirSync(staticDest)
   for (const file of svgFiles) {
-    svgtopdf(path.join(staticDest, file))
+    try {
+      svgtopdf(path.join(staticDest, file))
+    } catch (error) {
+      console.error(`Something went wrong while parsing file ${file} through svgomg resulting in error: ${error}`)
+    }
   }
 
   // Generate zip archives for svg
