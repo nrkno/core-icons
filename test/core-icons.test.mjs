86cd2fbd7f618f37802864172fe7eb2b77907a6b
Skjalg Teig <skjalgepalg@gmail.com>
2023-10-09 14:48:07 +0200

    test: verify importing js exports works before versioning

---
+++ test/core-icons.test.mjs
@@ -0,0 +1,13 @@
+// Test package exports using testpack-cli
+import * as icons from '@nrk/core-icons'
+import * as expressive from '@nrk/core-icons/expressive'
+import * as logos from '@nrk/core-icons/logo'
+import * as previews from '@nrk/core-icons/preview'
+
+console.info(`
+Package contains the following:\n
+ - ${Object.keys(icons).length} icons\n
+ - ${Object.keys(expressive).length} expressive icons\n
+ - ${Object.keys(logos).length} logos\n
+ - ${Object.keys(previews).length} logos\n
+`)
