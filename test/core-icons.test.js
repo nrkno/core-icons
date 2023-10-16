390ebf7d12c7c435ccd2dd66d2280a4ecb3a2d75
Skjalg Teig <skjalgepalg@gmail.com>
2023-10-16 11:40:43 +0200

    test: verify large format logos for jsx and vanilla in js and mjs

--- test/core-icons.test.js
+++ test/core-icons.test.js
@@ -2,6 +2,7 @@
 const icons = require('@nrk/core-icons')
 const expressive = require('@nrk/core-icons/expressive')
 const logos = require('@nrk/core-icons/logo')
+const largeLogos = require('@nrk/core-icons/logo/large')
 const previews = require('@nrk/core-icons/preview')
 
 console.info(`
@@ -9,5 +10,6 @@ Package contains the following for commonJS:\n
  - ${Object.keys(icons).length} icons\n
  - ${Object.keys(expressive).length} expressive icons\n
  - ${Object.keys(logos).length} logos\n
+ - ${Object.keys(largeLogos).length} large format logos\n
  - ${Object.keys(previews).length} previews\n
 `)
