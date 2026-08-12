8aa29b15e8fa7ddb06aa07646fad103736abeb55
gesi <helge.silset@nrk.no>
2026-08-12 12:23:34 +0200

    fix docs base url

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import pkg from '../package.json' with { type: 'json' }
 
-export default defineConfig({
-  build: {
-    outDir: '../static',
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? `/core-icons/${pkg.version}/` : '/',
+    build: {
+      outDir: '../static',
+    },
+  }
 })
